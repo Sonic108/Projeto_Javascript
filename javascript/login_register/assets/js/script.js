de18d4a90fcc3572639ea96fd2e76e54be7afd70
Miranha <00001126465239sp@al.educacao.sp.gov.br>
2025-08-28 14:27:38 -0300

    Criamos

--- javascript/login_register/assets/js/script.js
+++ javascript/login_register/assets/js/script.js
@@ -1,5 +1,26 @@
 const btnLogin = document.querySelector("#btn-login");
 const btnRegister = document.querySelector("#btn-register");
-btnLogin.addEventListener("cilck", (e) => {
-    console.log("Cliquei no botão login"); 
+const modalLogin = document.querySelector("#modal-login")
+const modalRegister = document.querySelector("#modal-register")
+const btnCancelLogin = document.querySelector("#btn-cancel-login")
+const btnCancelRegister = document.querySelector("#btn-cancel-register")
+btnLogin.addEventListener("click", (e) => {
+    modalLogin.classList.remove("hide-modal");
+    modalLogin.classList.add("show-modal");
+})
+btnRegister.addEventListener("click", (e) => {
+    modalRegister.classList.remove("hide-modal");
+    modalRegister.classList.add("show-modal");
+})
+btnCancelLogin.addEventListener("click", (e) => {
+    modalLogin.classList.add("hide-modal");
+    modalLogin.classList.remove("show-modal");
+})
+btnCancelRegister.addEventListener("click", (e) => {
+    modalRegister.classList.add("hide-modal");
+    modalRegister.classList.remove("show-modal");
+})
+btnCancelRegister.addEventListener("click", (e) => {
+    modalRegister.classList.add("hide-modal");
+    modalRegister.classList.remove("show-modal");
 })
